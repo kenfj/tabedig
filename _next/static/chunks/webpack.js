25a3a719e011944929e2b77da4003134c61e539d
Ken Fujii <ken.fj@icloud.com>
2021-04-04 17:12:08 +0900

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee26bf853b07bc165b35";
+/******/ 	var hotCurrentHash = "b855ca39c782da3ff0a4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- api/__api__users.js
+++
@@ -1,4 +0,0 @@
-const { adaptor } = require('next-export-api')
-const { default: handler } = require('../../.next/server/pages/api/users.js')
-
-module.exports.handler = adaptor(handler)
